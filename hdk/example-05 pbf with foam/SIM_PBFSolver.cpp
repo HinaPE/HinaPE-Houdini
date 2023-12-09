99d2b290399fa25e633de399953662711efa6dfa
Xayah-Hina <ycxian14sep@gmail.com>
2023-12-09 21:01:44 +0900

    add guide geometry fw

--- hdk/example-05 pbf with foam/SIM_PBFSolver.cpp
+++ hdk/example-05 pbf with foam/SIM_PBFSolver.cpp
@@ -14,6 +14,8 @@
 #include <SIM/SIM_Collider.h>
 #include <SIM/SIM_ColliderPoint.h>
 #include <SIM/SIM_RelationshipSource.h>
+#include <SIM/SIM_PRMShared.h>
+#include <SIM/SIM_GuideShared.h>
 
 #include <GU/GU_Detail.h>
 #include <GA/GA_Handle.h>
@@ -31,16 +33,12 @@ const SIM_DopDescription *SIM_PBFSolver::GetDescription()
 	static PRM_Name gravity("gravity", "Gravity");
 	static std::array<PRM_Default, 3> gravity_default = {0, -9.8, 0};
 
-	static PRM_Name stiffness("stiffness", "Stiffness");
-	static PRM_Default stiffness_default(1);
+	static PRM_Name dt("dt", "Dt");
+	static PRM_Default dt_default(1e-3);
 
-	static PRM_Name constraint_iteration("constraint_iteration", "Constraint Iteration");
-	static PRM_Default constraint_iteration_default(3);
-
-	static std::array<PRM_Template, 4> PRMS{
+	static std::array<PRM_Template, 3> PRMS{
 			PRM_Template(PRM_FLT_J, 3, &gravity, gravity_default.data()),
-			PRM_Template(PRM_FLT_J, 1, &stiffness, &stiffness_default),
-			PRM_Template(PRM_INT_J, 1, &constraint_iteration, &constraint_iteration_default),
+			PRM_Template(PRM_FLT_J, 1, &dt, &dt_default),
 			PRM_Template()
 	};
 
@@ -50,6 +48,21 @@ const SIM_DopDescription *SIM_PBFSolver::GetDescription()
 								   "PBFSolver",
 								   classname(),
 								   PRMS.data());
+
+	static PRM_Name boundary("boundary", "Boundary");
+	static std::array<PRM_Default, 3> boundary_default = {2, 2, 2};
+
+	static std::array<PRM_Template, 4> GuidePRMS{
+			PRM_Template(PRM_TOGGLE, 1, &SIMshowguideName,
+						 PRMzeroDefaults),
+			PRM_Template(PRM_RGB, 3,
+						 &SIMcolorName, PRMoneDefaults,
+						 0, &PRMunitRange),
+			PRM_Template(PRM_FLT_J, 3, &boundary, boundary_default.data()),
+			PRM_Template()
+	};
+
+	DESC.setGuideTemplates(GuidePRMS.data());
 	return &DESC;
 }
 
@@ -68,7 +81,7 @@ SIM_Solver::SIM_Result SIM_PBFSolver::solveSingleObjectSubclass(SIM_Engine &, SI
 
 	SIM_GeometryCopy *geo = SIM_DATA_GET(object, "Geometry", SIM_GeometryCopy);
 	SIM_GeometryAutoWriteLock lock(geo);
-	GU_Detail& gdp = lock.getGdp();
+	GU_Detail &gdp = lock.getGdp();
 
 	std::cout << "obj points offset: ";
 	for (GA_Size i = 0; i < gdp.getNumPoints(); ++i)
@@ -87,8 +100,8 @@ SIM_Solver::SIM_Result SIM_PBFSolver::solveSingleObjectSubclass(SIM_Engine &, SI
 		if (!affector.getName().equal(object.getName()))
 		{
 			SIM_Geometry *collider_geo = SIM_DATA_GET(affector, "Geometry", SIM_Geometry);
-			SIM_GeometryAutoReadLock lock(collider_geo);
-			const GU_Detail* collider_gdp = lock.getGdp();
+			SIM_GeometryAutoReadLock collider_lock(collider_geo);
+			const GU_Detail *collider_gdp = collider_lock.getGdp();
 
 			std::cout << "collider points offset: ";
 			for (GA_Size i = 0; i < collider_gdp->getNumPoints(); ++i)
@@ -99,7 +112,7 @@ SIM_Solver::SIM_Result SIM_PBFSolver::solveSingleObjectSubclass(SIM_Engine &, SI
 			std::cout << "\n";
 
 			GU_Detail *merged_gdp = new GU_Detail();
-			merged_gdp->merge(gdp);
+			merged_gdp->merge(gdp); // Track offsets By yourself
 			merged_gdp->merge(*collider_gdp);
 
 			std::cout << "merged points offset: ";
@@ -112,4 +125,17 @@ SIM_Solver::SIM_Result SIM_PBFSolver::solveSingleObjectSubclass(SIM_Engine &, SI
 		}
 	}
 	return SIM_Solver::SIM_SOLVER_SUCCESS;
+}
+
+SIM_Guide *SIM_PBFSolver::createGuideObjectSubclass() const
+{
+	// Return a shared guide so that we only have to build our geometry
+	// once. But set the displayonce flag to false so that we can set
+	// a different transform for each object.
+	return new SIM_GuideShared(this, false);
+}
+
+void SIM_PBFSolver::buildGuideGeometrySubclass(const SIM_RootData &root, const SIM_Options &options, const GU_DetailHandle &gdh, UT_DMatrix4 *xform, const SIM_Time &t) const
+{
+	SIM_Data::buildGuideGeometrySubclass(root, options, gdh, xform, t);
 }
--- hdk/example-05 pbf with foam/SIM_PBFSolver.h
+++ hdk/example-05 pbf with foam/SIM_PBFSolver.h
@@ -11,14 +11,21 @@ class SIM_PBFSolver : public SIM_SingleSolver, public SIM_OptionsUser
 {
 public:
 	GETSET_DATA_FUNCS_V3("gravity", Gravity);
-	GETSET_DATA_FUNCS_F("stiffness", Stiffness);
-	GETSET_DATA_FUNCS_I("constraint_iteration", ConstraintIteration);
+	GETSET_DATA_FUNCS_I("dt", Dt);
+
+	GET_GUIDE_FUNC_B(SIM_NAME_SHOWGUIDE, ShowGuide, true);
+	GET_GUIDE_FUNC_V3(SIM_NAME_COLOR, Color, (1, 1, 1));
+	GET_GUIDE_FUNC_V3("boundary", Boundary, (2, 2, 2));
 
 private:
 	SIM_PBFSolver(const SIM_DataFactory *factory) : SIM_SingleSolver(factory), SIM_OptionsUser(this) {}
 	~SIM_PBFSolver() override = default;
 	SIM_Result solveSingleObjectSubclass(SIM_Engine &engine, SIM_Object &object, SIM_ObjectArray &feedbacktoobjects, const SIM_Time &timestep, bool newobject) override;
 	static const SIM_DopDescription *GetDescription();
+
+protected:
+	SIM_Guide *createGuideObjectSubclass() const override;
+	void buildGuideGeometrySubclass(const SIM_RootData &root, const SIM_Options &options, const GU_DetailHandle &gdh, UT_DMatrix4 *xform, const SIM_Time &t) const override;
 
 DECLARE_STANDARD_GETCASTTOTYPE();
 DECLARE_DATAFACTORY(SIM_PBFSolver, SIM_SingleSolver, "PBF Solver Description", GetDescription());
