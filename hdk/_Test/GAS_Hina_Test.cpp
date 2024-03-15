86d31ef6617173690435265c22809878f62af36f
Xayah <ycxiang14sep@gmail.com>
2024-03-16 04:02:37 +0900

    update

--- hdk/_Test/GAS_Hina_Test.cpp
+++ hdk/_Test/GAS_Hina_Test.cpp
@@ -2,32 +2,24 @@
 
 #include <Base/utils.h>
 
+//#include "DataTypes.h"
+//#include "Field.h"
+//#include "Collision/CollisionDetectionBroadPhase.h"
+
 GAS_HINA_SUBSOLVER_IMPLEMENT(
 		Test,
 		true,
 		false,
 )
-void GAS_Hina_Test::_init()
-{
-	this->InnerPtr = nullptr;
-}
-void GAS_Hina_Test::_makeEqual(const GAS_Hina_Test *src)
-{
-	this->InnerPtr = src->InnerPtr;
-}
+void GAS_Hina_Test::_init() {}
+void GAS_Hina_Test::_makeEqual(const GAS_Hina_Test *src) {}
 bool GAS_Hina_Test::_solve(SIM_Engine &engine, SIM_Object *obj, SIM_Time time, SIM_Time timestep)
 {
-	if (!InnerPtr)
+	static bool init = false;
+	if (!init)
 	{
-		SIM_Geometry *SOPGeometry = SIM_DATA_GET(*obj, SIM_GEOMETRY_DATANAME, SIM_Geometry);
-		if (!SOPGeometry)
-			throw std::runtime_error("No geometry found for " + std::string(SIM_GEOMETRY_DATANAME));
-		UT_Vector3 pos;
-		std::pair<std::vector<Vector>, std::vector<size_t>> triangle_mesh_info = ReadTriangleMeshFromGeometry<double, Vector>(SOPGeometry, pos);
-		const std::vector<Vector> &V = triangle_mesh_info.first;
-		const std::vector<size_t> &I = triangle_mesh_info.second;
-		this->InnerPtr = std::make_shared<Surface>(V, I);
-		InnerPtr->update_transform(pos, Quaternion{0, 0, 0 ,1});
+//		using namespace dyno;
+//		CollisionDetectionBroadPhase<DataType3f> collision;
 	}
 	return true;
 }
--- hdk/_Test/GAS_Hina_Test.h
+++ hdk/_Test/GAS_Hina_Test.h
@@ -3,17 +3,8 @@
 
 #include <SIM_Hina_Generator.h>
 
-#include "common/geometry.h"
-using real = float;
-using Vector = UT_Vector3;
-using Quaternion = UT_Quaternion;
-using Surface = HinaPE::ISurface<real, Vector, Quaternion>;
-using SurfacePtr = std::shared_ptr<Surface>;
-
 GAS_HINA_SUBSOLVER_CLASS(
 		Test,
-
-		SurfacePtr InnerPtr;
 )
 
 #endif //HINAPE_GAS_HINA_TEST_H
