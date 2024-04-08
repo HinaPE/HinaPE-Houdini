02bc4169bd046d9c4b114b7d50e9b5f3de71c3b4
Xayah <ycxiang14sep@gmail.com>
2024-04-09 01:18:02 +0900

    update smoke solver

---
+++ HinaPE/Smoke/SmokeNew.h
@@ -0,0 +1,17 @@
+#ifndef HINAPE_SMOKENEW_H
+#define HINAPE_SMOKENEW_H
+
+namespace HinaPE
+{
+struct SmokeParam
+{
+
+};
+
+struct SmokeNew : public SmokeParam
+{
+
+};
+}
+
+#endif //HINAPE_SMOKENEW_H
