afe2aa1203fccc7d76ce307d18b6a8cbc542b099
Xayah <ycxiang14sep@gmail.com>
2024-04-08 17:07:03 +0900

    update test

--- hdk/_Test/GAS_Hina_Test.cpp
+++ hdk/_Test/GAS_Hina_Test.cpp
@@ -1,25 +1,33 @@
 #include "GAS_Hina_Test.h"
 
-#include <Base/utils.h>
-
-//#include "DataTypes.h"
-//#include "Field.h"
-//#include "Collision/CollisionDetectionBroadPhase.h"
+#include <Smoke/FieldUtils.h>
+#include <CE/CE_Grid.h>
+#include <iostream>
 
 GAS_HINA_SUBSOLVER_IMPLEMENT(
 		Test,
 		true,
 		false,
+		ACTIVATE_GAS_VELOCITY \
+        ACTIVATE_GAS_DENSITY
 )
 void GAS_Hina_Test::_init() {}
 void GAS_Hina_Test::_makeEqual(const GAS_Hina_Test *src) {}
 bool GAS_Hina_Test::_solve(SIM_Engine &engine, SIM_Object *obj, SIM_Time time, SIM_Time timestep)
 {
-	static bool init = false;
-	if (!init)
+	SIM_ScalarField *D = getScalarField(obj, GAS_NAME_DENSITY);
+//	SIM_VectorField *V = getVectorField(obj, GAS_NAME_VELOCITY);
+//
+//	if (!V->isFaceSampled())
+//		return false;
+
 	{
-//		using namespace dyno;
-//		CollisionDetectionBroadPhase<DataType3f> collision;
+		const SIM_RawField &Field = *D->getField();
+		auto res = HinaPE::ToCubby(Field);
+
+		std::cout << "Field.getVoxelRes(): " << Field.getVoxelRes() << std::endl;
+		std::cout << "Field.getVoxelSize(): " << Field.getVoxelSize() << std::endl;
+		std::cout << "Field.getOrig(): " << Field.getOrig() << std::endl;
 	}
 	return true;
 }
